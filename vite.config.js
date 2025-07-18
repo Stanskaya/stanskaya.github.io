1c9ea291f08a60803debfe281cc3c2eab2d55eca
ksenia <xenast98@gmail.com>
2025-07-18 20:29:56 +0300

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-    base: '/ceramica/',
+    base: '/',
 
     build: {
         rollupOptions: {
