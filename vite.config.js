dcd855a7bd6c71956447bb05594ffb4fd6ffc729
ksenia <xenast98@gmail.com>
2025-07-18 21:03:49 +0300

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
+import { resolve } from 'path'
 
 export default defineConfig({
-    base: '',
+    base: '', // или './'
 
     build: {
         rollupOptions: {
