84a307e18df3455639bb41e2b1987e56cefc90e3
Stanskaya <151540130+Stanskaya@users.noreply.github.com>
2025-07-19 13:07:47 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -8,46 +8,6 @@ const swiper = new Swiper('.swiper', {
 import '/src/sass/style.scss';
 import '../sass/style.scss';
 
-const burger = document.querySelector(".burger"),
-    close = document.querySelector(".header__menu-close"),
-    menu = document.querySelector(".header__menu");
-
-burger.addEventListener("click", () => {
-    menu.classList.add("header__menu__active");
-    document.body.style.overflow = "hidden";
-});
-
-close.addEventListener("click", () => {
-    menu.classList.remove("header__menu__active");
-    document.body.style.overflow = "";
-});
-
-try {
-    new Swiper('.works__slider', {
-        slidesPerView: 1,
-        loop: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".icon-right-open",
-            prevEl: ".icon-left-open",
-        },
-        breakpoints: {
-            1200: {
-                slidesPerView: 3,
-                spaceBetween: 5
-            },
-            1920: {
-                spaceBetween: 35
-            },
-        },
-    });
-} catch (e) {
-    console.error("Swiper error", e);
-}
-
 
 const burger = document.querySelector(".burger"),
     close = document.querySelector(".header__menu-close"),
