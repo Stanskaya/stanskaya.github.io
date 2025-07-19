7018e5134dc76ea807c220d186cea3efba475190
Stanskaya <151540130+Stanskaya@users.noreply.github.com>
2025-07-19 13:10:17 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
+// Importlar en üstte olmalı
+import Swiper, { Navigation, Pagination } from 'swiper';
+import '/src/sass/style.scss';
+import '../sass/style.scss';
+
+// Ana swiper
 const swiper = new Swiper('.swiper', {
   loop: true,
   pagination: {
@@ -5,13 +11,10 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-import '/src/sass/style.scss';
-import '../sass/style.scss';
-
-
+// Menü açma-kapama
 const burger = document.querySelector(".burger"),
-    close = document.querySelector(".header__menu-close"),
-    menu = document.querySelector(".header__menu");
+      close = document.querySelector(".header__menu-close"),
+      menu = document.querySelector(".header__menu");
 
 burger.addEventListener("click", () => {
     menu.classList.add("header__menu__active");
@@ -23,7 +26,7 @@ close.addEventListener("click", () => {
     document.body.style.overflow = "";
 });
 
-
+// works__slider swiper'ı (try-catch ile hata önlemek için)
 try {
     new Swiper('.works__slider', {
         slidesPerView: 1,
@@ -33,46 +36,43 @@ try {
             clickable: true,
         },
         navigation: {
-            // nextEl: ".icon-right-open",
-            // prevEl: ".icon-left-open",
             nextEl: ".icon-right-open",
             prevEl: ".icon-left-open",
         },
         breakpoints: {
-            // when window width is >= 1200px
             1200: {
                 slidesPerView: 3,
                 spaceBetween: 5
             },
-            // when window width is >= 1920px
             1920: {
                 spaceBetween: 35
             },
-
         },
         modules: [Navigation, Pagination],
     });
-} catch (e) { }
+} catch (e) {
+    // Hata varsa sessizce yut
+}
 
+// Katalog tab sistemi
 try {
     const tabs = document.querySelectorAll(".catalog__tab");
     const contents = document.querySelectorAll(".catalog__content-item");
 
     tabs.forEach((tab, index) => {
         tab.addEventListener("click", () => {
-            // Удаляем активный класс у всех табов и контента
             tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
             contents.forEach((c) => (c.style.display = "none"));
 
-            // Добавляем активный класс к нажатому табу и показываем соответствующий контент
             tab.classList.add("catalog__tab_active");
             contents[index].style.display = "flex";
         });
     });
 
-    // Показываем первый контент при загрузке
+    // İlk içerik gösterimi
     contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
-} catch (e) { }
+} catch (e) {
+    // Hata varsa sessizce yut
+}
 
-// Обратите внимание, что значение block (в двух местах) можно спокойно поменять на flex, если вам это необходимо
-
+// Not: "display: block" olan yerleri ihtiyacına göre "flex" yapabilirsin.
